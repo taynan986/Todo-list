f547eaf9e35096edcbe37c9001f4f4d2e3e29774
taynan986 <70826076+taynan986@users.noreply.github.com>
2025-04-01 10:57:48 -0300

    Add files via upload

---
+++ mysql/db.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE `database` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+
+CREATE TABLE `todolist` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `status` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
